4a798df0306295e5a5868fc69d98f69f949a3f2d
piratf <948970803@qq.com>
2016-01-09 16:28:51 +0800

    complete context of three libraries

---
+++ ContextConsoleViaSDL/main.cpp
@@ -0,0 +1,31 @@
+#include <SDL.h>
+#include <SDL_opengl.h>
+
+int main(int argc, char *argv[])
+{
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+
+	SDL_Window* window = SDL_CreateWindow("OpenGL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+	SDL_Event windowEvent;
+
+	while (true)
+	{
+		if (SDL_PollEvent(&windowEvent))
+		{
+			if (windowEvent.type == SDL_QUIT) break;
+			if (windowEvent.type == SDL_KEYUP &&
+				windowEvent.key.keysym.sym == SDLK_ESCAPE) break;
+		}
+		SDL_GL_SwapWindow(window);
+	}
+
+	SDL_GL_DeleteContext(context);
+
+	SDL_Quit();
+	return 0;
+}
