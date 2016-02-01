b07478153a3e99744a0e964ec5714226fe077744
piratf <948970803@qq.com>
2016-02-01 21:12:28 +0800

    ready to push

--- ContextConsoleViaSDL/main.cpp
+++ ContextConsoleViaSDL/main.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char *argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
-
+    r
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
